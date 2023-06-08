a808ebef6c37069485f4a935aa6b65d54bff4f0c
AnatStauber <anat.s.goren@gmail.com>
2023-06-08 10:11:59 +0300

    start

---
+++ src/components/counter.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+export default class Counter extends Component{
+
+    state = {counter:0};
+
+    add1 = () => {
+        if (this.state.counter<=4){
+            this.setState({counter:this.state.counter+1});
+        }
+        else {
+            this.setState({counter:0});
+        }
+    }
+
+render() {
+    return (
+        <div>
+            <h2 class="text-success text-decoration-underline"> assignment 1:</h2>
+            <h2> Counter : {this.state.counter}</h2>
+            <button onClick={this.add1} > + </button>
+        </div>
+    )
+}
+
+
+}
---
+++ src/components/gallery.js
@@ -0,0 +1,46 @@
+import { Component } from "react";
+
+export default class Gallery extends Component{
+
+
+    
+    state = {counter:0};
+
+    images_ar = ["images/parrot.jpg","images/pic2.jpg","images/pic3.jpg","images/pic4.jpg"];
+    
+    next = () => {
+        if (this.state.counter<=2){
+            this.setState({counter:this.state.counter+1});
+        }
+        else{
+            this.setState({counter:0});
+        }
+    }
+
+    back = () => {
+        if (this.state.counter>=1){
+            this.setState({counter:this.state.counter-1});
+        } else {
+            this.setState({counter:3})
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 class="text-success text-decoration-underline"> assignment 2:</h2>
+               <br></br>
+                <div class="">
+                    <div> 
+                    <img class="col-4" src={this.images_ar[this.state.counter]}></img>
+                    </div>
+                    <br></br>
+                <div class="col-4 d-flex justify-content-between">
+                <button  onClick={this.back}> Previous </button>
+                <button  onClick={this.next}> Next </button>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
